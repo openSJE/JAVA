5e44fb903790aae6c2f604686c05eae3936ea4e3
JE <sos19189@naver.com>
2022-09-28 08:43:35 +0900

    열거형클래스_WEEK

---
+++ Week.java
@@ -0,0 +1,24 @@
+package chapter05;
+
+/**
+ * packageName : chapter05
+ * fileName : week
+ * author : ds
+ * date : 2022-09-27
+ * description : 요일을 저장할 열거형 클래스
+ * ===========================================================
+ * DATE            AUTHOR             NOTE
+ * —————————————————————————————
+ * 2022-09-27         ds          최초 생성
+ */
+// 요일이 정의된 열거형: 상수(고정된 숫자, 문자열 등 정의), 변수(변할 수 있는 숫자, 문자열)
+// 얼거형 : 상수를 정의하는 곳
+public enum Week {
+    MONDAY,
+    TUESDAY,
+    WEDNESDAY,
+    THURSDAY,
+    FRIDAY,
+    SATURDAY,
+    SUNDAY
+}
