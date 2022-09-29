88be4fd18f089c461aa8cf9804c88106935cc7e6
JE <sos19189@naver.com>
2022-09-29 09:03:43 +0900

    기본

---
+++ Member.java
@@ -0,0 +1,19 @@
+package chapter06.sec02.exam03;
+
+/**
+ * packageName : Verify06
+ * fileName : Member
+ * author : ds
+ * date : 2022-09-27
+ * description :
+ * ===========================================================
+ * DATE            AUTHOR             NOTE
+ * —————————————————————————————
+ * 2022-09-27         ds          최초 생성
+ */
+public class Member {
+    String name;
+    String id;
+    String password;
+    int age;
+}
